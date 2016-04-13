dcddf42687d1bccf40287db452cc2d71e3721eee
Jeff Lockhart <jwlockhart@users.noreply.github.com>
2016-04-12 21:21:24 -0400

    sorted nodes in tripartite graph

--- examples/draw_tripartite.py
+++ examples/draw_tripartite.py
@@ -59,43 +59,48 @@ for c in code_cols:
 
 #add each excerpt of text as a node. Connect it with relevant 
 #students and codes.
+st = []
+ex = []
+last = ''
 for row in df.iterrows():
+    #add the student node
     g.add_node(row[1]['s_id'], t='student')
+    #if we haven't seen this student before, save the order we saw them in
+    if last != row[1]['s_id']:
+        last = row[1]['s_id']
+        st.append(last)
+    
+    #add this excerpt node. Save its order to our list.
     g.add_node(row[1]['e_id'], t='excerpt')
+    ex.append(row[1]['e_id'])
+    
+    #add the edge joining this student and excerpt.
     g.add_edge(row[1]['s_id'], row[1]['e_id'])
-
+    
+    #for each code this excerpt has, draw an edge to it
     for c in code_cols:
         if row[1][c]:
             g.add_edge(row[1]['e_id'], c)
-            
-
-#get a list of each type of node
-students = [n for (n, d) in g.nodes(data=True) if 
-            (d['t'] == 'student')]
-excerpts = [n for (n, d) in g.nodes(data=True) if 
-            (d['t'] == 'excerpt')]
-codes = [n for (n, d) in g.nodes(data=True) if  
-         (d['t'] == 'code')]
 
 #get a dictionary of our code nodes' labels
 l = {}
-for c in codes:
+for c in code_cols:
     l[c] = c
 
 #fix the positions of each node type in columns
 pos = dict()
 #space out the student and code nodes to align with excerpt column height
-pos.update( (n, (1, i*5.5)) for i, n in enumerate(students) )
-pos.update( (n, (2, i)) for i, n in enumerate(excerpts) ) 
-pos.update( (n, (3, i*90)) for i, n in enumerate(codes) ) 
+pos.update( (n, (1, i*5.57)) for i, n in enumerate(st) )
+pos.update( (n, (2, i)) for i, n in enumerate(ex) ) 
+pos.update( (n, (3, i*90)) for i, n in enumerate(code_cols) ) 
 
 #make our figure big so we can see
 plt.figure(figsize=(20,20))
 
 #draw our nodes
-nx.draw_networkx_nodes(g, pos, nodelist=students, node_color='r', 
+nx.draw_networkx_nodes(g, pos, nodelist=st, node_color='r', 
                       node_shape='^')
-nx.draw_networkx_nodes(g, pos, nodelist=excerpts, node_color='b', 
+nx.draw_networkx_nodes(g, pos, nodelist=ex, node_color='b', 
                       node_shape='o', alpha=0.5)
 #draw our edges with low alpha so we can see
 nx.draw_networkx_edges(g, pos, alpha=0.2)
